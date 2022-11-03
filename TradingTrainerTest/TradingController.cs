f422efa051ce2cf728d01e5882604cc4c4375318
okabmoss <s331690@oslomet.no>
2022-11-03 15:00:17 +0100

    metoder som skal testes

---
+++ TradingTrainerTest/TradingController.cs
@@ -0,0 +1,101 @@
+﻿namespace TradingTrainerTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public async Task GetAllTrades()  // 1
+    {
+
+    }
+
+
+    [Fact]
+    public async Task GetStockAsync()  // 2
+    {
+
+    }
+
+    [Fact]
+    public async Task GetStockQuoteAsync()  // 3
+    {
+
+    }
+
+    [Fact]
+    public async Task SellStockTransactionAsync() // 4
+    {
+
+    }
+
+    [Fact]
+    public async Task BuyStockTransactionAsync() // 5
+    {
+
+    }
+
+    [Fact]
+    public async Task RemoveStockQuotes() // 6
+    {
+
+    }
+
+    [Fact]
+    public async Task GetUserAsync() // 7
+    {
+
+    }
+
+    [Fact]
+    public async Task AddStockQuoteAsync() // 8
+    {
+
+    }
+
+    [Fact]
+    public async Task GetFavoriteListAsync() // 9
+    {
+
+    }
+
+    [Fact]
+    public async Task AddToFavoriteListAsync() // 10
+    {
+
+    }
+
+    [Fact]
+    public async Task DeleteFromFavoriteListAsync() // 11
+    {
+
+    }
+
+    [Fact]
+    public async Task GetUsersAsync() // 12
+    {
+
+    }
+
+    [Fact]
+    public async Task ResetPortfolio() // 13
+    {
+
+    }
+
+    [Fact]
+    public async Task UpdateUserAsync() // 14
+    {
+
+    }
+    [Fact]
+    public async Task ClearAllTradeHistoryAsync() // 15
+    {
+
+    }
+    [Fact]
+    public async Task CleanTradingSchemaAsync() // 16
+    {
+
+    }
+  
+
+}
