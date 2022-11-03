6563f9496fb31d4b345fd78f76b4e5f37b8953ed
okabmoss <s331690@oslomet.no>
2022-11-04 00:19:56 +0100

    metoder vi bør teste

---
+++ TradingTrainerTest/TradingControllerTest.cs
@@ -0,0 +1,195 @@
+﻿namespace TradingTrainerTest
+{
+    public class TradingControllerTest
+    {
+        [Fact]
+        public async Task Getportfolio_LogetInn_Ok()
+        {
+            // Arange
+
+
+            // Act
+
+
+            // Assert
+
+        }
+        [Fact]
+        public async Task Getportfolio_LogetInn_Feil()
+        {
+
+        }
+        [Fact]
+        public async Task Getportfolio_IkkeLogetInn()
+        {
+
+        }
+
+
+
+        [Fact]
+        public async Task GetFavoriteList_LogetInn_Ok()
+        {
+
+        }
+        [Fact]
+        public async Task GetFavoriteList_LogetInn_Feil()
+        {
+
+        }
+        [Fact]
+        public async Task GetFavoriteList_IkkeLogetInn()
+        {
+
+        }
+
+
+
+        [Fact]
+        public async Task GetStockQuote_LogetInn_Ok()
+        {
+
+        }
+        [Fact]
+        public async Task GetStockQuote_LogetInn_Feil()
+        {
+
+        }
+        [Fact]
+        public async Task GetStockQuote_IkkeLogetInn()
+        {
+
+        }
+
+
+
+        [Fact]
+        public async Task GetUserSearchResult_LogetInn_Ok()
+        {
+
+        }
+        [Fact]
+        public async Task GetUserSearchResult_LogetInn_Feil()
+        {
+
+        }
+        [Fact]
+        public async Task GetUserSearchResult_IkkeLogetInn()
+        {
+
+        }
+
+
+
+        [Fact]
+        public async Task AddToFavoriteList_LogetInn_Ok()
+        {
+
+        }
+        [Fact]
+        public async Task AddToFavoriteList_LogetInn_Feil()
+        {
+
+        }
+        [Fact]
+        public async Task AddToFavoriteList_IkkeLogetInn()
+        {
+
+        }
+
+
+
+        [Fact]
+        public async Task DeleteFromFavoriteList_LogetInn_Ok()
+        {
+
+        }
+        [Fact]
+        public async Task DeleteFromFavoriteList_LogetInn_Feil()
+        {
+
+        }
+        [Fact]
+        public async Task DeleteFromFavoriteList_IkkeLogetInn()
+        {
+
+        }
+
+
+
+        [Fact]
+        public async Task GetAllTrades_LogetInn_Ok()
+        {
+
+        }
+        [Fact]
+        public async Task GetAllTrades_LogetInn_Feil()
+        {
+
+        }
+        [Fact]
+        public async Task GetAllTrades_IkkeLogetInn()
+        {
+
+        }
+
+
+
+        [Fact]
+        public async Task ClearAllTradeHistory_LogetInn_Ok()
+        {
+
+        }
+        [Fact]
+        public async Task ClearAllTradeHistory_LogetInn_Feil()
+        {
+
+        }
+        [Fact]
+        public async Task ClearAllTradeHistory_IkkeLogetInn()
+        {
+
+        }
+
+
+
+        [Fact]
+        public async Task ResetProfile_LogetInn_Ok()
+        {
+
+        }
+        [Fact]
+        public async Task ResetProfile_LogetInn_Feil()
+        {
+
+        }
+        [Fact]
+        public async Task ResetProfile_IkkeLogetInn()
+        {
+
+        }
+
+
+
+        [Fact]
+        public async Task GetUser_LogetInn_Ok()
+        {
+
+        }
+        [Fact]
+        public async Task GetUser_LogetInn_Feil()
+        {
+
+        }
+        [Fact]
+        public async Task GetUser_IkkeLogetInn()
+        {
+
+        }
+
+
+    }
+
+
+}
+
