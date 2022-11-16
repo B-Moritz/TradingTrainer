b00c763b7010208404f634758987504e8d122739
okabmoss <s331690@oslomet.no>
2022-11-16 12:53:59 +0100

    footter

--- TradingTrainer/ClientApp/src/Footer.tsx
+++ TradingTrainer/ClientApp/src/Footer.tsx
@@ -8,10 +8,24 @@ function Footer(): JSX.Element {
                 <ul>
                     <li>
                         <a target="_blank" href="https://icons8.com/icon/DnhznTvrGRgU/trading">Trading</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
-
                     </li>
                 </ul>
             </div>
+            <div>
+                <ul>
+                    <li>
+                        <a target="_blank" href="https://icons8.com/icon/DnhznTvrGRgU/trading">Links</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <a target="_blank" href="https://icons8.com/icon/DnhznTvrGRgU/trading">Notice</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+                    </li>
+                </ul>
+            </div>
+
 
             <ul>
                 <li>
